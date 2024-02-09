995cd38ef260cdb681b7b6a491b005567620ca34
Tookssi <pjubullet816@gmail.com>
2024-02-10 04:23:11 +0900

    safeAutKit used

--- src/Safe_API.tsx
+++ src/Safe_API.tsx
@@ -2,9 +2,9 @@
 import {
     SafeAuthPack,
     SafeAuthInitOptions,
-    AuthKitSignInData
+    AuthKitSignInData,
 } from '@safe-global/auth-kit';
-
+import Safe, { EthersAdapter } from '@safe-global/protocol-kit';
 import { ethers } from 'ethers';
 
 let signInData: AuthKitSignInData;
@@ -20,21 +20,71 @@ const safeAuthInitOptions: SafeAuthInitOptions = {
 };
 
 const safeAuthPack = new SafeAuthPack();
-// await safeAuthPack.init(safeAuthInitOptions);
+await safeAuthPack.init(safeAuthInitOptions);
 
 async function handleSignInOnSafeWallet() {
     console.log("sign in to safe wallet");
-    // signInData = await safeAuthPack.signIn();
+    signInData = await safeAuthPack.signIn();
 }
 
 async function handleSignOutOnSafeWallet() {
     console.log("sign out from safe wallet");
-
+    await safeAuthPack.signOut();
 }
 
-function handleExecuteTransaction() {
+async function handleExecuteTransaction() {
     console.log("execute transaction");
+    
+    if (!signInData?.safes?.length) {
+        console.error('No safes found for this the current signer');
+        return;
+    }
 
+    // Wrap the adapter
+    provider = new ethers.providers.Web3Provider(
+        safeAuthPack.getProvider() as ethers.providers.ExternalProvider
+    );
+    const signer = provider.getSigner();
+
+    // Create the Safe EthersAdapter
+    const ethAdapter = new EthersAdapter({
+        ethers,
+        signerOrProvider: signer || provider
+    });
+
+    // Create the Safe protocol kit instance
+    const protocolKit = await Safe.create({
+        ethAdapter,
+        safeAddress: signInData?.safes?.[0] || '', // Use a Safe with funds
+    });
+    
+    const initialBalance = await protocolKit.getBalance();
+    console.log(
+        `The initial balance of the Safe: ${ethers.utils.formatUnits(
+          initialBalance,
+          'ether'
+        )} xDAI`
+      );
+      
+    // Create a Safe transaction with the provided parameters
+    const tx = await protocolKit.createTransaction({
+        safeTransactionData: {
+          to: signInData?.eoa || '0x', // Address to send the funds from the Safe
+          data: '0x',
+          value: ethers.utils.parseUnits('0.0001', 'ether').toString(),
+        },
+      });
+  
+    await protocolKit.executeTransaction(tx);
+
+    const finalBalance = await protocolKit.getBalance();
+
+    console.log(
+    `The final balance of the Safe: ${ethers.utils.formatUnits(
+        finalBalance,
+        'ether'
+    )} xDAI`
+    );
 }
 
 function Safe_API() {
