41a2f2382f1bb90fb61736914956f735307366c3
Tookssi <pjubullet816@gmail.com>
2024-02-09 16:45:54 +0900

    server port setting remove

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   optimizeDeps: {
     disabled: false,
     esbuildOptions: {
