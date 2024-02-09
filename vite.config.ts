e825f769ad0130338024d95de2e91173f9be1eb0
Tookssi <pjubullet816@gmail.com>
2024-02-09 16:43:00 +0900

    Uncaught ReferenceError: process is not defined  at node_modules/pump/index.js (@safe-global_auth-kit.js?v=ea2a8cb4:15547:33)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  optimizeDeps: {
+    disabled: false,
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  // define: {
+  //   'process.env': {},
+  //   global: {},
+  // },
 })
