51eab249eebc9381a4bbe491d21e92438059fda7
Tookssi <pjubullet816@gmail.com>
2024-02-09 16:38:57 +0900

    new SafeAuthPack

--- src/Safe_API.tsx
+++ src/Safe_API.tsx
@@ -1,9 +1,33 @@
 
-function handleSignInOnSafeWallet() {
+import {
+    SafeAuthPack,
+    SafeAuthInitOptions,
+    AuthKitSignInData
+} from '@safe-global/auth-kit';
+
+import { ethers } from 'ethers';
+
+let signInData: AuthKitSignInData;
+let provider: ethers.providers.Web3Provider;
+
+const safeAuthInitOptions: SafeAuthInitOptions = {
+    enableLogging: true,
+    showWidgetButton: false,
+    chainConfig: {
+        rpcTarget: 'https://gnosis.drpc.org',
+        chainId: '0x64',
+    },
+};
+
+const safeAuthPack = new SafeAuthPack();
+// await safeAuthPack.init(safeAuthInitOptions);
+
+async function handleSignInOnSafeWallet() {
     console.log("sign in to safe wallet");
+    // signInData = await safeAuthPack.signIn();
 }
 
-function handleSignOutOnSafeWallet() {
+async function handleSignOutOnSafeWallet() {
     console.log("sign out from safe wallet");
 
 }
